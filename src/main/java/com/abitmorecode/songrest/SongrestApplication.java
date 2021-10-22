f493ed355ee8af9427a9a60197f71e87aad25e4d
kev <ks1202210@gmail.com>
2021-10-22 19:00:07 +0200

    Add Get Mappings for specific Song and all Songs

--- src/main/java/com/abitmorecode/songrest/SongrestApplication.java
+++ src/main/java/com/abitmorecode/songrest/SongrestApplication.java
@@ -1,13 +1,36 @@
 package com.abitmorecode.songrest;
 
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SongrestApplication {
+
+	private static Gson gson = new Gson();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SongrestApplication.class, args);
 	}
 
+	@GetMapping("/ABitMoreCode/songs/{id}")
+	public String getSong(@PathVariable String id){
+		int songId = 0;
+		try {
+			songId = Integer.parseInt(id);
+		}catch(NumberFormatException e){
+			// TODO: HTMLresponse with error code
+			return "";
+		}
+		return gson.toJson(SongController.instance.getSpecificSong(songId));
+	}
+
+	@GetMapping("/ABitMoreCode/songs")
+	public String getSongs(){
+		return gson.toJson(SongController.instance.getAllSongs());
+	}
 }
--- src/test/java/com/abitmorecode/songrest/SongrestApplicationTests.java
+++ src/test/java/com/abitmorecode/songrest/SongrestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.abitmorecode.songrest;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SongrestApplicationTests {
 
+	private static SongrestApplication songrestApplication;
+
+	@BeforeAll
+	static void beforeAll() {
+		songrestApplication = new SongrestApplication();
+	}
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void getSong() {
+
+	}
+
+	@Test
+	void getSongs() {
+
+	}
 }
