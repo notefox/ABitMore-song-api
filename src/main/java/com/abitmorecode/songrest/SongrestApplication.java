0ffaf56922f34428ae5dcdc8d4d8234c1c6c929f
kev <ks1202210@gmail.com>
2021-10-22 18:00:00 +0200

    initial spring commit

---
+++ src/main/java/com/abitmorecode/songrest/SongrestApplication.java
@@ -0,0 +1,13 @@
+package com.abitmorecode.songrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abitmorecode/songrest/SongrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abitmorecode.songrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
