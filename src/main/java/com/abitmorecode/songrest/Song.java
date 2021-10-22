2989a91dacfb1b644ca17304a4dbea73abc74d0d
note <kitsu-geissler@t-online.de>
2021-10-22 18:30:34 +0200

    add most basic Song Controller methods

--- src/main/java/com/abitmorecode/songrest/Song.java
+++ src/main/java/com/abitmorecode/songrest/Song.java
@@ -6,4 +6,44 @@ public class Song {
 	private String artist;
 	private String label;
 	private int released;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public int getReleased() {
+		return released;
+	}
+
+	public void setReleased(int released) {
+		this.released = released;
+	}
 }
--- src/main/java/com/abitmorecode/songrest/SongController.java
+++ src/main/java/com/abitmorecode/songrest/SongController.java
@@ -1,5 +1,34 @@
 package com.abitmorecode.songrest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SongController {
 
+	public static SongController instance = new SongController();
+	private final List<Song> songs = new ArrayList<>();
+
+	public void init(String filepath) {
+		// do the stuff
+	}
+
+	public Song getSpecificSong(int id) {
+		return (Song) songs.stream().filter(s -> s.getId() == id);
+	}
+
+	public Song[] getAllSongs() {
+		return songs.toArray(new Song[]{});
+	}
+
+	public void addSong(Song song) {
+		songs.add(song);
+	}
+
+	public void addSong(String json) {
+		// TODO: do this
+	}
+
+	public void deleteSong(int id) {
+		songs.remove((Song) songs.stream().filter(s -> s.getId() == id));
+	}
 }
