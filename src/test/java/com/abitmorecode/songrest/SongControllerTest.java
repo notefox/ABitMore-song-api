8ea20be05a415485ec3905db9b8e9222a6403da9
note <kitsu-geissler@t-online.de>
2021-10-24 03:38:36 +0200

    add more tests

--- src/test/java/com/abitmorecode/songrest/SongControllerTest.java
+++ src/test/java/com/abitmorecode/songrest/SongControllerTest.java
@@ -1,5 +1,8 @@
 package com.abitmorecode.songrest;
 
+import com.abitmorecode.songrest.SongControllerException.SameSongAlreadyExistException;
+import com.abitmorecode.songrest.SongControllerException.SongIdAlreadyExistException;
+import com.google.gson.JsonParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,10 +26,19 @@ class SongControllerTest {
 
 	@BeforeEach
 	void setup() {
+		SongController.instance.reset();
 		correctpath = path + "correct_songs.json";
 		wrongpath = path + "incorrect.json";
 		weirdpath = path + "incorrect_songs.json";
 		nonepath = path + "somepath";
+	}
+
+	void init() {
+		try {
+			SongController.instance.init(correctpath);
+		} catch (IOException | SongController.AlreadyInitializedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
@@ -44,16 +57,68 @@ class SongControllerTest {
 	void basicInitializeTest() {
 		try {
 			SongController.instance.init(correctpath);
-		} catch (IOException e) {
+		} catch (IOException | SongController.AlreadyInitializedException e) {
 			fail();
 		}
 
-		Song[] songs = SongController.instance.getAllSongs();
-		assertEquals(new Song(1, "Das Test", "N bisschen Test", "TestTestTest", 2015).toString(), songs[0].toString());
+		Song[] songs = new Song[0];
+		try {
+			songs = SongController.instance.getAllSongs();
+		} catch (SongController.NotYetInitializedException e) {
+			fail();
+		}
+		assertEquals(new Song(2, "Das Test", "N bisschen Test", "TestTestTest", 2015).toString(), songs[1].toString());
 	}
 
 	@Test
 	void pathNotFoundTest() {
 		assertThrows(NoSuchFileException.class, () -> SongController.instance.init(nonepath));
+	}
+
+	@Test
+	void fileHasNoJsonInItTest() {
+		assertThrows(JsonParseException.class, () -> SongController.instance.init(wrongpath));
+	}
+
+	@Test
+	void fileHasWrongJsonInItTest() {
+		assertThrows(JsonParseException.class, () -> SongController.instance.init(weirdpath));
+	}
+
+	@Test
+	void addASimpleSong() {
+		init();
+		Song song = new Song(2, "tite", "artist", "label", 2020);
+		try {
+			SongController.instance.addSong(song);
+		} catch (SameSongAlreadyExistException | SongController.NotYetInitializedException e) {
+			fail();
+		}
+		try {
+			assertEquals(Arrays.stream(SongController.instance.getAllSongs()).findFirst().get().toString(), song.toString());
+		} catch (SongController.NotYetInitializedException e) {
+			fail();
+		}
+	}
+
+	@Test
+	void addASongWithOriginalParamList() {
+		init();
+		Song song = new Song(2, "tite", "artist", "label", 2020);
+		try {
+			SongController.instance.addSong(2, "tite", "artist", "label", 2020);
+		} catch (SongController.NotYetInitializedException | SongIdAlreadyExistException e) {
+			fail();
+		}
+		try {
+			assertEquals(Arrays.stream(SongController.instance.getAllSongs()).findFirst().get().toString(), song.toString());
+		} catch (SongController.NotYetInitializedException e) {
+			fail();
+		}
+	}
+
+	@Test
+	void addASongWithAutomaticIdAssigment() {
+		init();
 	}
 }
