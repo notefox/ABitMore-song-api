eaa568e2bbcd9c1f6da736095fd83e29c245c52d
note <kitsu-geissler@t-online.de>
2021-10-27 02:05:47 +0200

    SongrestApplication is now called SongController, since the name is more fitting for a RestController

---
+++ src/main/java/com/abitmorecode/songrest/Controller/SongController.java
@@ -0,0 +1,61 @@
+package com.abitmorecode.songrest.Controller;
+
+import com.abitmorecode.songrest.Models.Song;
+import com.abitmorecode.songrest.SongControllerException.SameSongAlreadyExistException;
+import com.abitmorecode.songrest.SongControllerException.SongDoesntExistException;
+import com.abitmorecode.songrest.SongControllerException.SongIdAlreadyExistException;
+import com.abitmorecode.songrest.Services.SongsManager;
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SongController {
+	private static final Logger log = LoggerFactory.getLogger(SongController.class);
+
+	@Autowired
+	private SongsManager songService;
+	private static final Gson gson = new Gson();
+
+	@GetMapping("/ABitMoreCode/songs/{id}")
+	public String getSong(@PathVariable int songId) {
+		try {
+			return gson.toJson(songService.getSpecificSong(songId));
+		} catch (SongDoesntExistException e) {
+			// TODO: use Logger
+			e.printStackTrace();
+			return e.toString();
+		}
+	}
+
+	@GetMapping("/ABitMoreCode/songs")
+	public String getSongs() {
+		return gson.toJson(songService.getAllSongs());
+	}
+
+	@PostMapping("/ABitMoreCode/songs")
+	public Song postSong(@RequestBody Song song) {
+		try {
+			songService.addSong(song);
+			return song;
+		} catch (SameSongAlreadyExistException | SongIdAlreadyExistException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@DeleteMapping("/ABitMoreCode/songs/{id}")
+	public int deleteSong(@PathVariable int id) {
+		try {
+			songService.deleteSong(id);
+			return id;
+		} catch (SongDoesntExistException | NumberFormatException e) {
+			e.printStackTrace();
+		}
+
+		// TODO: HTML response with error code
+		return -1;
+	}
+}
