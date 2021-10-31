472f623173136b7b965dedf50861a8a6b8bae299
note <kitsu-geissler@t-online.de>
2021-10-31 09:55:06 +0100

    give Exceptions a Response Status

--- src/main/java/com/abitmorecode/songrest/SongControllerException/SameSongAlreadyExistException.java
+++ src/main/java/com/abitmorecode/songrest/SongControllerException/SameSongAlreadyExistException.java
@@ -1,5 +1,10 @@
 package com.abitmorecode.songrest.SongControllerException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.CONFLICT, reason="song already exists")
 public class SameSongAlreadyExistException extends Exception {
 	public SameSongAlreadyExistException() {
 		super();
